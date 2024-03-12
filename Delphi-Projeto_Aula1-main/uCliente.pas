b0e035747737cbc55b91b2fb1962194e2bd5eb35
AllanUnser <147109701+AllanUnser@users.noreply.github.com>
2024-03-11 22:09:02 -0300

    Add files via upload

--- Delphi-Projeto_Aula1-main/uCliente.pas
+++ Delphi-Projeto_Aula1-main/uCliente.pas
@@ -3,18 +3,19 @@ unit uCliente;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Edit,
   FMX.Controls.Presentation, FMX.StdCtrls, FMX.ListBox, FMX.Memo.Types,
   FMX.ScrollBox, FMX.Memo;
 
 type
-TCliente = record
-  codigo, idade : Integer;
-  nome, cidade, sobrenome, sexo : string;
-  ativo : Boolean;
-  salario : Float32;
-end;
+  TCliente = record
+    codigo, idade: Integer;
+    nome, cidade, sobrenome, sexo: string;
+    ativo: Boolean;
+    salario: Float32;
+  end;
 
   Tfrm_cliente = class(TForm)
     Label1: TLabel;
@@ -36,12 +37,16 @@ end;
     btn_limpar: TButton;
     btn_tirarCarteira: TButton;
     Memo1: TMemo;
+    ImprimirTodos: TButton;
+    CarregarCliente: TButton;
     procedure btn_gravarClick(Sender: TObject);
     procedure validarCampos();
     procedure limparCampos();
     procedure btn_limparClick(Sender: TObject);
     procedure btn_tirarCarteiraClick(Sender: TObject);
-    procedure inserirClienteLista(cliente : TCliente);
+    procedure inserirClienteLista(cliente: TCliente);
+    procedure ImprimirTodosClick(Sender: TObject);
+    procedure CarregarClienteClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -50,16 +55,19 @@ end;
 
 var
   frm_cliente: Tfrm_cliente;
+  aCliente: Array [0 .. 10] of TCliente;
 
 implementation
+
 {$R *.fmx}
 
 procedure Tfrm_cliente.btn_gravarClick(Sender: TObject);
-var vCliente : TCliente;
+var
+  vCliente: TCliente;
 begin
   validarCampos;
 
-  //atribuindo os valores
+  // atribuindo os valores
   vCliente.codigo := StrToInt(edt_codigo.Text);
   vCliente.nome := edt_nome.Text;
   vCliente.sobrenome := edt_sobrenome.Text;
@@ -81,27 +89,74 @@ procedure Tfrm_cliente.btn_limparClick(Sender: TObject);
 begin
   limparCampos;
 end;
+
 procedure Tfrm_cliente.btn_tirarCarteiraClick(Sender: TObject);
 begin
-{
-  if idade >= 18 then
+  {
+    if idade >= 18 then
     ShowMessage('Pode tirar carteira')
-  else
-    ShowMessage('Não pode tirar carteira.');}
+    else
+    ShowMessage('Não pode tirar carteira.'); }
 end;
 
+procedure Tfrm_cliente.CarregarClienteClick(Sender: TObject);
+var
+  codigoBusca, I: Integer;
+begin
+  codigoBusca := StrToInt(edt_codigo.Text);
+  for I := 0 to 10 do
+  begin
+    if codigoBusca = aCliente[I].codigo then
+    begin
+      Memo1.Lines.Add('Cliente Encontrado');
+      Memo1.Lines.Add('Cliente cod: ' + IntToStr(aCliente[I].codigo));
+      Memo1.Lines.Add('Nome completo: ' + aCliente[I].nome + '' + aCliente[I]
+        .sobrenome);
+      Memo1.Lines.Add('Idade: ' + IntToStr(aCliente[I].idade));
+      Memo1.Lines.Add('Cidade: ' + aCliente[I].cidade);
+      Memo1.Lines.Add('Salario: ' + FloatToStr(aCliente[I].salario));
+      Memo1.Lines.Add('Ativo: ' + aCliente[I].ativo.ToString());
+      Memo1.Lines.Add('======================');
+      break;
+    end;
 
+  end;
 
-procedure Tfrm_cliente.inserirClienteLista(cliente: TCliente);
+end;
+
+procedure Tfrm_cliente.ImprimirTodosClick(Sender: TObject);
+var
+  I: Integer;
 begin
 
-    Memo1.Lines.Add('Cliente cod: ' + IntToStr(cliente.codigo));
-    Memo1.Lines.Add('Nome completo: ' + cliente.nome +''+ cliente.sobrenome);
-    Memo1.Lines.Add('Idade: ' + IntToStr(cliente.idade));
-    Memo1.Lines.Add('Cidade: ' + cliente.cidade);
-    Memo1.Lines.Add('Salario: ' + FloatToStr(cliente.salario));
-    Memo1.Lines.Add('Ativo: '+cliente.ativo.ToString());
-    Memo1.Lines.Add('======================');
+  for I := 0 to 10 do
+  begin
+    if aCliente[I].codigo <> 0 then
+    begin
+      Memo1.Lines.Add('Cliente cod: ' + IntToStr(aCliente[I].codigo));
+      Memo1.Lines.Add('Nome completo: ' + aCliente[I].nome + '' + aCliente[I]
+        .sobrenome);
+      Memo1.Lines.Add('Idade: ' + IntToStr(aCliente[I].idade));
+      Memo1.Lines.Add('Cidade: ' + aCliente[I].cidade);
+      Memo1.Lines.Add('Salario: ' + FloatToStr(aCliente[I].salario));
+      Memo1.Lines.Add('Ativo: ' + aCliente[I].ativo.ToString());
+      Memo1.Lines.Add('======================');
+
+    end;
+
+  end;
+end;
+
+procedure Tfrm_cliente.inserirClienteLista(cliente: TCliente);
+var
+  I: Integer;
+begin
+  for I := 0 to 10 do
+    if aCliente[I].codigo = 0 then
+    begin
+      aCliente[I] := cliente;
+      break;
+    end;
 end;
 
 procedure Tfrm_cliente.limparCampos;
@@ -112,6 +167,7 @@ begin
   edt_salario.Text := '';
   edt_cidade.Text := '';
 end;
+
 procedure Tfrm_cliente.validarCampos;
 begin
   if edt_codigo.Text = '' then
@@ -153,4 +209,5 @@ begin
   end;
 
 end;
+
 end.
